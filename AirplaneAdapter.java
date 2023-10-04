b235103b7c93923f69e2ba859683c1618120c608
Abhi Patel <apatel477@myseneca.ca>
2023-10-04 00:56:19 -0400

    Added AirplaneAdapter ~ Abhi

--- AirplaneAdapter.java
+++ AirplaneAdapter.java
@@ -1,3 +1,37 @@
-public class AirplaneAdapter extends Vehicle{
-    
+public class AirplaneAdapter extends Vehicle {
+    private Airplane airplane;
+
+    public AirplaneAdapter(Airplane airplane) {
+        this.airplane = airplane;
+    }
+
+    @Override
+    public void addFuel(double amount) {
+        airplane.airplaneAddFuel(amount);
+    }
+
+    @Override
+    public double getDistance() {
+        return airplane.getAirplaneDistance();
+    }
+
+    @Override
+    public double getTime() {
+        return airplane.getAirplaneTime();
+    }
+
+    @Override
+    public double getFuelLevel() {
+        return airplane.getAirplaneFuelLevel();
+    }
+
+    @Override
+    public double getFuelCost() {
+        return airplane.getAirplaneFuelCost();
+    }
+
+    @Override
+    public void travel(double distance) {
+        airplane.airplaneTravel(distance);
+    }
 }
