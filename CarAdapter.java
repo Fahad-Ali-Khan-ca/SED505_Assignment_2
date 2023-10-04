61bb21978272b6dfeb4c344c6eb650e2ebe227bc
Abhi Patel <apatel477@myseneca.ca>
2023-10-04 00:54:59 -0400

    Added CarAdapter ~ Abhi

---
+++ CarAdapter.java
@@ -0,0 +1,37 @@
+public class CarAdapter extends Vehicle {
+    private Car car;
+
+    public CarAdapter(Car car) {
+        this.car = car;
+    }
+
+    @Override
+    public void addFuel(double amount) {
+        car.carAddFuel(amount);
+    }
+
+    @Override
+    public double getDistance() {
+        return car.getCarDistance();
+    }
+
+    @Override
+    public double getTime() {
+        return car.getCarTime();
+    }
+
+    @Override
+    public double getFuelLevel() {
+        return car.getCarFuelLevel();
+    }
+
+    @Override
+    public double getFuelCost() {
+        return car.getCarFuelCost();
+    }
+
+    @Override
+    public void travel(double distance) {
+        car.carTravel(distance);
+    }
+}
