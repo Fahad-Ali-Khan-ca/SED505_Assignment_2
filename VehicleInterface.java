f0b98d38c0bc8c1c557a45b0fe79b7b59f401ef5
Fahad Ali Khan <fkhan195@myseneca.ca>
2023-10-07 11:42:28 -0400

    Revert "Removed VehicleInterface file because it was unnecessary"
    This reverts commit 029409ae6a58530bce9657b4e72962095bc24350.

---
+++ VehicleInterface.java
@@ -0,0 +1,8 @@
+public class VehicleInterface {
+    void addFuel(double amount);
+    void travel(double distance);
+    double getDistance();
+    double getTime();
+    double getFuelLevel();
+    double getFuelCost();
+}
