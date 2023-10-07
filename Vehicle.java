9b2e4ccd22faa78d87ae0fe40f59ae25fe4b50ae
Abhi Patel <apatel477@myseneca.ca>
2023-10-07 16:29:45 -0400

    Final commit

--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,5 @@
+//An interface/abstract class representing the 3 vehicles.
+//Modified: Abhi Patel, Fahad ALi Khan, Inderpreet Parmar
 public interface Vehicle {
     // Complete the class
     void addFuel(double amount);
